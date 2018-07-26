c77aa032355a9aa10ebbfca030ac475144484f1c
Jaewook Kwon <somedd@naver.com>
2018-07-26 13:45:53 +0900

    Feat: Add a BrandCollectionPageControl. #2

--- DrinKit/DrinKit/AppDelegate.swift
+++ DrinKit/DrinKit/AppDelegate.swift
@@ -10,13 +10,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font: UIFont.tabbarText], for: .normal)
+        setTabbarFont()
         return true
     }
-
+    
+    private func setTabbarFont() {
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font: UIFont.tabbarText], for: .normal)
+    }
+    
 }
 

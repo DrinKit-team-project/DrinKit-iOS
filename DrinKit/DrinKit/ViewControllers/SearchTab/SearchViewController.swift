36d02ec11fc558cbc5fedb5793b282d881f38f0f
ChocOZerO <nayalth@gmail.com>
2018-07-26 17:58:53 +0900

    [Add]SearchBar Controller Contents

--- DrinKit/DrinKit/ViewControllers/SearchTab/SearchViewController.swift
+++ DrinKit/DrinKit/ViewControllers/SearchTab/SearchViewController.swift
@@ -2,34 +2,38 @@
 //  SearchViewController.swift
 //  DrinKit
 //
-//  Created by 권재욱 on 2018. 7. 26..
-//  Copyright © 2018년 권재욱. All rights reserved.
+//  Created by JakeLEE on 2018. 7. 26..
+//  Copyright © 2018년 ChocOZerO. All rights reserved.
 //
 
 import UIKit
 
 class SearchViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var recommendScrollView: UIScrollView!
+    @IBOutlet weak var topReviewCountButton: UIButton!
+    @IBOutlet weak var topReviewImageStackView: UIStackView!
+    @IBOutlet weak var newMenuCountButton: UIButton!
+    @IBOutlet weak var newMenuImageStackView: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.searchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
     }
-    */
 
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = false
+        self.view.endEditing(true)
+    }
 }

557e6220ce248b03a60874c71c7a1f4880959463
Jaewook Kwon <somedd@naver.com>
2018-07-27 17:31:55 +0900

    Fix: Edit constraints of BeverageDetailView. #5

--- DrinKit/DrinKit/ViewControllers/HomeTab/BeverageDetailViewController.swift
+++ DrinKit/DrinKit/ViewControllers/HomeTab/BeverageDetailViewController.swift
@@ -10,46 +10,34 @@ import UIKit
 
 class BeverageDetailViewController: UIViewController {
     @IBOutlet weak var beverageDetailScrollView: UIScrollView!
+    @IBOutlet weak var thumbnailsScrollView: UIScrollView!
     @IBOutlet weak var reviewTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         reviewTableView.delegate = self
         reviewTableView.dataSource = self
-//        reviewTableView.estimatedRowHeight = reviewTableView.rowHeight
-//        reviewTableView.rowHeight = UITableViewAutomaticDimension
-//        
+        reviewTableView.estimatedRowHeight = reviewTableView.rowHeight
+        reviewTableView.rowHeight = UITableViewAutomaticDimension
     }
     
-    //    func configureSelfHeight() {
-    //        beverageDetailScrollView.contentSize.height =  UIScreen.main.bounds.height + reviewTableView.contentSize.height
-    //    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        //                beverageDetailScrollView.contentSize.height = UIScreen.main.bounds.height + reviewTableView.contentSize.height
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        beverageDetailScrollView.contentSize.height = view.frame.height + reviewTableView.contentSize.height
-        //        configureSelfHeight()
-    }
+
     
 }
 
 extension BeverageDetailViewController: UITableViewDelegate, UITableViewDataSource {
     
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let reviewCell = tableView.dequeueReusableCell(withIdentifier: "ReviewCell") as? ReviewCell else { return UITableViewCell() }
         return reviewCell
     }
-    
-    
     
 }

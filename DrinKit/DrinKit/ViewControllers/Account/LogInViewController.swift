f306e92dbf9f9e6e59326a7caf0b9e4b50ebd00b
Jaewook Kwon <somedd@naver.com>
2018-07-30 12:02:02 +0900

    Feat: Add a button Accessing Facebook. #8

--- DrinKit/DrinKit/ViewControllers/Account/LogInViewController.swift
+++ DrinKit/DrinKit/ViewControllers/Account/LogInViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class LogInViewController: UIViewController {
 
@@ -14,5 +15,31 @@ class LogInViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    @IBAction func loginFacebook(_ sender: UIButton){
+        let fbLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["public_profile", "email", "user_friends"], from: self) { (result, error) in
+            if (error == nil) {
+                let fbloginresult : FBSDKLoginManagerLoginResult = result!
+                if(fbloginresult.grantedPermissions.contains("email"))
+                {
+                    self.getFBUserData()
+                }
+            }
+        }
+    }
+    
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    //everything works print the user data
+                    print(result as Any)
+                    self.performSegue(withIdentifier: "ToSettings", sender: self)
+                }
+            })
+        }
+    }
 
 }
+
+

ca3411fa0e6a7f931ec2d22afb7d345019352371
Jaewook Kwon <somedd@naver.com>
2018-08-06 15:20:36 +0900

    Feat: Add LoginFlowHandler. #11

--- DrinKit/DrinKit/AppDelegate.swift
+++ DrinKit/DrinKit/AppDelegate.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+
 import FBSDKCoreKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, LoginFlowHandler {
     
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        setTabbarFont()
         
+//        window = UIWindow.init(frame: UIScreen.main.bounds)
+//        initialiseServices()
+//        handleLogin(withWindow: window)
+        
+        setTabbarFont()
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions) || true
     }
     
@@ -37,16 +42,68 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if KOSession.isKakaoAccountLoginCallback(url) {
             return KOSession.handleOpen(url)
         }
-        
         let facebookSession = FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
         return facebookSession
     }
     
     private func setTabbarFont() {
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font: UIFont.tabbarText], for: .normal)
+    UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font: UIFont.tabbarText], for: .normal)
     }
     
 }
+
+// MARK: - LoginFlowHandler
+
+enum AppStoryboard: String {
+    
+    case main = "Main"
+    case account = "Account"
+    
+    var instance: UIStoryboard {
+        return UIStoryboard(name: self.rawValue, bundle: Bundle.main)
+    }
+    
+}
+
+protocol LoginFlowHandler {
+    
+    func handleLogin(withWindow window: UIWindow?)
+    func handleLogout(withWindow window: UIWindow?)
+    
+}
+
+extension LoginFlowHandler {
+    
+    func handleLogin(withWindow window: UIWindow?) {
+        
+//        if let _ = AppState.shared.currentUserId {
+//            //User has logged in before, cache and continue
+//            self.showMainApp(withWindow: window)
+//        } else {
+//            //No user information, show login flow
+//            self.showLogin(withWindow: window)
+//        }
+    }
+    
+    func handleLogout(withWindow window: UIWindow?) {
+//        AppState.shared.signOut()
+        showLogin(withWindow: window)
+    }
+    
+    func showLogin(withWindow window: UIWindow?) {
+        window?.subviews.forEach { $0.removeFromSuperview() }
+        window?.rootViewController = nil
+        window?.rootViewController = AppStoryboard.account.instance.instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+    }
+    
+    func showMainApp(withWindow window: UIWindow?) {
+        window?.rootViewController = nil
+        window?.rootViewController = AppStoryboard.main.instance.instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
 
 
 

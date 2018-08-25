57bfe4f858514eb0d94de2e418e26a87c088c73d
Jaewook Kwon <somedd@naver.com>
2018-08-25 13:50:24 +0900

    Feat: Add Cafes. #19

--- DrinKit/DrinKit/Model/Cafes.swift
+++ DrinKit/DrinKit/Model/Cafes.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import Alamofire
+
+class Cafes {
+    
+    var allCafes = [Cafe]()
+    var count: Int {
+        return allCafes.count
+    }
+    subscript(index : Int) -> Cafe {
+        return allCafes[index]
+    }
+    
+    init() {
+        Alamofire.request("").responseJSON { (response) in
+            guard let data = response.data else { return }
+            self.allCafes = self.decode(data)
+        }
+    }
+    
+    private func decode(_ data : Data) -> [Cafe] {
+        do {
+            return try JSONDecoder().decode([Cafe].self, from : data)
+        } catch {
+            return []
+        }
+    }
+    
+}
